dde17ecb35ff8013ccad08884bc57e22de086039
Dashulietta <108624114+Dashulietta@users.noreply.github.com>
2022-10-27 13:32:09 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
      <h1> Weather App </h1>
       <footer>
-      This project is created by <a href ="https://brilliant-kelpie-907866.netlify.app" target="_blank"  rel="noreferrer" > Petrova Dariia </a> and it is 
+      This project is created by <a href ="https://brilliant-kelpie-907866.netlify.app" target="_blank"  rel="noreferrer"> Petrova Dariia </a> and it is 
       <a href="https://github.com/Dashulietta/weather-react-app" target="_blank"  rel="noreferrer" > open-sorced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
+
+
 
 
