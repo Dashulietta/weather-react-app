59c52f06cfde72bce1226ad64a211289f0ce1d9b
Dashulietta <108624114+Dashulietta@users.noreply.github.com>
2022-10-27 12:03:08 +0300

    added links github and netlify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
   <div className="App">
-      Weather App
+     <h1> Weather App </h1>
+      <footer>
+      This project is created by <a href ="https://brilliant-kelpie-907866.netlify.app" target="_blank"  rel="noreferrer" > Petrova Dariia </a> and it is 
+      <a href="https://github.com/Dashulietta/weather-react-app" target="_blank"  rel="noreferrer" > open-sorced on GitHub</a>
+      </footer>
     </div>
   );
 }
