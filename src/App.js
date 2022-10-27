e4c53f412bcd60508fbd660019af9d4481e7d0ed
Dashulietta <108624114+Dashulietta@users.noreply.github.com>
2022-10-27 13:54:02 +0300

    Created html content in Weather.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Weather from "./Weather"
 
 export default function App() {
   return (
   <div className="App">
     <div className="container">
-     <h1> Weather App </h1>
+   
+     <Weather />
       <footer>
       This project is created by <a href ="https://brilliant-kelpie-907866.netlify.app" target="_blank"  rel="noreferrer"> Petrova Dariia </a> and it is 
       <a href="https://github.com/Dashulietta/weather-react-app" target="_blank"  rel="noreferrer" > open-sorced on GitHub</a>
