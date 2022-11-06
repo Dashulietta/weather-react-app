dfa4400e713fdd7bb794985fd755fd5d3da40cf2
Dashulietta <108624114+Dashulietta@users.noreply.github.com>
2022-11-06 11:41:57 +0200

    made app responsive

--- src/Weather.js
+++ src/Weather.js
@@ -56,7 +56,7 @@ function handleCityChange(event) {
                     onChange ={handleCityChange}/>
                     </div>
                     <div className="col-3"> 
-                    <input type="submit" value="Search" className="btn btn-primary w-100" />
+                    <input type="submit" value="Search" className="btn btn-primary w-80" />
                     </div>
                     </div>
                 </form>
